daf0a52f13f41899739c420a6ff04c2088707429
bride <toolsbrider@gmail.com>
2026-09-15 18:52:17 +0500

    disable caching for admin routes

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
   robots: { index: false, follow: false },
 };
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function AdminLayout({
   children,
 }: {
